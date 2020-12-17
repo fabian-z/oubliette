675d63dda3c208f07743d4ca071c994694288ca4
Fabian Zaremba <fabian@youremail.eu>
2020-12-17 18:31:12 +0100

    Implement weighted random item probability

--- src/item.js
+++ src/item.js
@@ -6,12 +6,15 @@ export class Item {
     effectCallback;
 }
 
+let weightedTypes = [];
+
 let types = [{
     name: 'Health Potion',
     symbol: "H",
     effect: function(game) {
         game.modifyPlayerHealth(getRandomInt(20, 100));
     },
+    probability: 0.7,
 }, {
     name: 'Agility Potion',
     symbol: "A",
@@ -21,6 +24,7 @@ let types = [{
             game.parameters.playerSpeed = game.defaultParameters.playerSpeed;
         }, 10 * 1000);
     },
+    probability: 0.2,
 }, {
     name: 'Monster Slowdown Totem',
     symbol: 'T',
@@ -32,6 +36,7 @@ let types = [{
             game.parameters.monsterInterval = game.defaultParameters.monsterInterval;
         }, 15 * 1000);
     },
+    probability: 0.1,
 }];
 
 export function getRandomType() {
@@ -41,7 +46,7 @@ export function getRandomType() {
 
 export function generateRandomItem() {
     let item = new Item();
-    let type = getRandomType();
+    let type = getSampledRandomType();
 
     item.type = type.name;
     item.symbol = type.symbol;
@@ -49,4 +54,25 @@ export function generateRandomItem() {
 
     //TODO randomize callback effect?
     return item;
+}
+
+// http://en.wikipedia.org/wiki/Rejection_sampling
+function initSampledRandom() {
+    for (let i of types) {
+        // factor should be large enough to convert smallest probability
+        // to decimal, e.g. for 0.1 -> 10, 0.01 -> 100, 0.001 -> 1000
+        let probWeight = i.probability * 10;
+        for (let j = 0; j < probWeight; j++) {
+            weightedTypes.push(i);
+        }
+    }
+}
+
+function getSampledRandomType() {
+    if (weightedTypes.length === 0) {
+        // allow items to be stateless, initialized on first request
+        initSampledRandom();
+    }
+    let i = getRandomInt(0, weightedTypes.length);
+    return weightedTypes[i];
 }
