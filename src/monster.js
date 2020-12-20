3496a14cce2bdd9f92dc60577fa7703da009741f
Fabian Zaremba <fabian@youremail.eu>
2020-12-20 23:13:38 +0100

    Decrease highest monster damage

--- src/monster.js
+++ src/monster.js
@@ -11,15 +11,15 @@ export class Monster {
     health = 100;
     damage;
     xp;
-    loot = [];
+    loot = []; // not yet implemented
 }
 
 let types = [
-    { name: 'Orc', symbol: "O", damage: 5, speed: 1 },
+    { name: 'Orc', symbol: "O", damage: 4, speed: 1 },
     { name: 'Bat', symbol: "B", damage: 1, speed: 1 },
     { name: 'Rat', symbol: "R", damage: 2, speed: 1 },
-    { name: 'Undead', symbol: "U", damage: 5, speed: 1 },
-    { name: 'Troll', symbol: "T", damage: 5, speed: 1 },
+    { name: 'Undead', symbol: "U", damage: 4, speed: 1 },
+    { name: 'Troll', symbol: "T", damage: 4, speed: 1 },
     { name: 'Warg', symbol: "W", damage: 3, speed: 1 },
     { name: 'Spider', symbol: "S", damage: 1, speed: 1 },
 ];
@@ -37,7 +37,7 @@ export function generateRandomMonster() {
     monster.type = type.name;
     monster.symbol = type.symbol;
     monster.speed = type.speed + getRandomInt(1, 5);
-    monster.damage = type.damage + getRandomInt(1, 5);
+    monster.damage = type.damage + getRandomInt(1, 3);
 
     monster.xp = getRandomInt(1, 10) + monster.speed + monster.damage;
 
