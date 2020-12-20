ef7c5395e0574edab2701a1f6fa6eb2af9f9dae2
Fabian Zaremba <fabian@youremail.eu>
2020-12-20 23:33:49 +0100

    Use monster base class speed

--- src/monster.js
+++ src/monster.js
@@ -16,12 +16,12 @@ export class Monster {
 
 let types = [
     { name: 'Orc', symbol: "O", damage: 4, speed: 1 },
-    { name: 'Bat', symbol: "B", damage: 1, speed: 1 },
+    { name: 'Bat', symbol: "B", damage: 1, speed: 2 },
     { name: 'Rat', symbol: "R", damage: 2, speed: 1 },
     { name: 'Undead', symbol: "U", damage: 4, speed: 1 },
     { name: 'Troll', symbol: "T", damage: 4, speed: 1 },
-    { name: 'Warg', symbol: "W", damage: 3, speed: 1 },
-    { name: 'Spider', symbol: "S", damage: 1, speed: 1 },
+    { name: 'Warg', symbol: "W", damage: 3, speed: 2 },
+    { name: 'Spider', symbol: "S", damage: 1, speed: 2 },
 ];
 
 export function getRandomType() {
@@ -36,7 +36,7 @@ export function generateRandomMonster() {
     let type = getRandomType();
     monster.type = type.name;
     monster.symbol = type.symbol;
-    monster.speed = type.speed + getRandomInt(1, 5);
+    monster.speed = type.speed + getRandomInt(1, 3);
     monster.damage = type.damage + getRandomInt(1, 3);
 
     monster.xp = getRandomInt(1, 10) + monster.speed + monster.damage;
